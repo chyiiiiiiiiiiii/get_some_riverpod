cbcb9ecceec387e9e07ef5472df9c52148e017e2
Yii <yii.chen@sensestar.live>
2022-04-14 16:47:01 +0800

    update test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,19 +1,32 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_some_riverpod/for_test/feed_page.dart';
 import 'package:get_some_riverpod/for_test/feed_repository.dart';
-import 'package:get_some_riverpod/main.dart';
 
 void main() {
-  testWidgets('FeedPage - Test', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      ProviderScope(
-        overrides: [feedsRepositoryProvider.overrideWithValue(MockFeedRepository())],
-        child: MyApp(),
-      ),
-    );
-    expect(find.textContaining('StateProvider'), findsOneWidget);
-    // expect(find.text('I am test 2'), findsNothing);
-    // expect(find.text('I am test 3'), findsOneWidget);
-  });
+
+  /// should see empty data in first frame, then get new mock-data after async operation in the second frame
+  /// Warn: can not make sure this process can pass in real situation, like data from server
+  testWidgets(
+    'FeedPage - Test',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: ProviderScope(
+            overrides: [feedsRepositoryProvider.overrideWithValue(MockFeedRepository())],
+            child: const FeedPage(),
+          ),
+        ),
+      );
+
+      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+      await tester.pump();
+
+      expect(find.text('I am Spiderman'), findsNothing);
+      expect(find.text('I am test'), findsOneWidget);
+      expect(find.text('I am test 2'), findsOneWidget);
+      expect(find.text('I am test 3'), findsOneWidget);
+    },
+  );
 }
